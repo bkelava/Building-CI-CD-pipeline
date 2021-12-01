8990df11f180dc7f2a10d586d4c042da940c9cdc
bkelava <59897801+bkelava@users.noreply.github.com>
2021-12-01 11:10:02 +0100

    Create commands.sh

---
+++ commands.sh
@@ -0,0 +1,11 @@
+cd CICD-Pipeline
+git clone https://github.com/wujenny15/Udacity-Build-CI-CD-Pipeline.git
+
+pip install virtualenv
+virtualenv ~/.azure-devops
+source ~/.udacity-devops/bin/activate
+deactivate
+
+az webapp up -n flaskmlwebapp
+
+az webapp log tail
